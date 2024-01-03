42ee7ca5470211b28227be0c577f2a8653d5004a
ngnlesn33 <son.nl215131@gmail.com>
2024-01-03 21:36:33 +0700

    Remove breadthFirstTraversal method from AbstractTree and Tree interfaces

--- sourcecode/Model/AbstractTree.java
+++ sourcecode/Model/AbstractTree.java
@@ -3,10 +3,6 @@ package Model;
 public abstract class AbstractTree<E> implements Tree<E> {
 
     @Override
-    /* Breadth-first traversal from the root */
-    public void breadthFirstTraversal() {}
-
-    @Override
     /* Return true if the tree is empty */
     public boolean isEmpty() {
         return getSize() == 0;
--- sourcecode/Model/Tree.java
+++ sourcecode/Model/Tree.java
@@ -5,31 +5,28 @@ public interface Tree<E> extends Iterable<E> {
     /**
      * Return true if the element is in the tree
      */
-    public boolean search(E e);
+    boolean search(E e);
 
     /**
      * Insert element o into the binary tree Return true if the element is inserted successfully
      */
-    public boolean insert(E e);
+    boolean insert(E e);
 
     /**
      * Delete the specified element from the tree Return true if the element is deleted successfully
      */
-    public boolean delete(E e);
+    boolean delete(E e);
 
-
-    // Breadth-first traversal from the root
-    public void breadthFirstTraversal();
 
     /**
      * Get the number of nodes in the tree
      */
 
-    public int getSize();
+    int getSize();
 
     /**
      * Return true if the tree is empty
      */
-    public boolean isEmpty();
+    boolean isEmpty();
 
 }
